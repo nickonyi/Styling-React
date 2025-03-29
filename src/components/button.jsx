68a92dd4fcd4129ff537c7b8c185bd6aebfe5ce5
phatommobb <nickoluoch1130@gmail.com>
2025-03-29 15:22:30 +0300

    styling a component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,3 +1,5 @@
+import styles from '../styles/Button.module.css';
+
 export default function Button() {
-  return <button>Submit</button>;
+  return <button className={styles.btn}>Submit</button>;
 }
